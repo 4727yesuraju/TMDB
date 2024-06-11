0da2e24a6e96eecacf3945b30f5bb92d6717f962
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 <4727yesuraju@gmail.com>
2024-06-11 17:10:56 +0530

    code

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4 text-justify">
+        <h1 className="text-2xl font-medium text-amber-600">About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis beatae ipsam obcaecati magnam expedita, aspernatur provident recusandae numquam eaque ab culpa illo cupiditate magni quasi, blanditiis nam ad unde nostrum sequi voluptatem sapiente perspiciatis! Eius distinctio at porro quibusdam. Hic delectus voluptatum maxime ducimus qui magnam! Vel quod rerum inventore!</p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae odio ipsa dolore, debitis pariatur architecto rem porro beatae dicta consequatur, repellat, incidunt nam ratione quia autem asperiores perspiciatis itaque? Cum libero, distinctio porro nisi ullam sed soluta blanditiis quam! Architecto animi earum quisquam optio, at dolor mollitia deserunt iste impedit voluptate itaque nemo quo deleniti, neque laborum, repellat praesentium pariatur? Cum, minus, quasi laboriosam maiores repellendus deleniti perspiciatis obcaecati consequatur incidunt neque a. Recusandae maiores laboriosam unde excepturi ratione nostrum eaque quo suscipit est ipsam corporis laborum distinctio sapiente cumque, quibusdam beatae, aperiam commodi exercitationem dolores repellendus esse! Quaerat, ut!</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis rem architecto officiis odio aspernatur cupiditate officia facilis beatae, veniam dolores voluptatibus esse necessitatibus perspiciatis cum corporis enim eum soluta. Dignissimos sequi doloremque necessitatibus quos nisi reiciendis voluptatum eos in fugiat!</p>
+    </div>
+  )
+}
